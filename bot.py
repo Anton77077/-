9a02229ed0234526a20db1c0aaf1620402b1c58e
Anton77077 <grizzli77077@gmail.com>
2024-09-25 17:14:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,12 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 # Токен бота
 TOKEN = "7602590385:AAHGMNyQGuNrQeJi8h2Ht-n0OMbMKEyt9zU"
 
+# URL Webhook Render
+WEBHOOK_URL = "https://srv-crq0m4l6l47c73arhegg.onrender.com"
+
 # Функция, которая отвечает на команду /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Бот работает.")
+    await update.message.reply_text("Привет! Бот работает на Webhook.")
 
 # Основная функция для запуска бота
 def main():
@@ -16,8 +19,13 @@ def main():
     # Добавляем обработчик команды /start
     application.add_handler(CommandHandler("start", start))
 
-    # Запуск бота
-    application.run_polling()
+    # Настраиваем Webhook
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=8443,
+        url_path=f"{TOKEN}",
+        webhook_url=f"{WEBHOOK_URL}/{TOKEN}"
+    )
 
 if __name__ == "__main__":
     main()
